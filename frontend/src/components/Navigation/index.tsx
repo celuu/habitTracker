af5b9f2fe66c1a5ad68e7882d18a876a185c4d5a
celuu <christine.e.luu@gmail.com>
2023-09-05 21:23:49 -0700

    Starting Nav Bar

---
+++ frontend/src/components/Navigation/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Navigation = () => {
+    return (
+        <>Nav Bar
+        <Link to='/hello'>Home</Link>
+        </>
+    )
+}
+
+export default Navigation;
